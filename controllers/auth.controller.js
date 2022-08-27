304f16a6a10d342af0ffd8c1a6ccbce653e9b2df
Marcin Lech <arcinech@gmail.com>
2022-08-27 21:22:37 +0200

    Fix: auth routes and controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,35 +1,41 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
+const getImageFileType = require('../utils/getImageFileType');
+const cleanFiles = require('../utils/cleanFiles');
 
 exports.register = async (req, res) => {
   try {
-    const { login, password, avatar, phoneNumber } = req.body;
+    const { login, password, phoneNumber } = req.body;
+    const fileType = req.file ? await getImageFileType(req.file) : 'unknown';
     if (
       login &&
       typeof login === 'string' &&
       password &&
       typeof password === 'string' &&
-      avatar &&
-      typeof avatar === 'string' &&
+      req.file &&
+      ['image/png', 'image/jpeg', 'image/gif'].includes(fileType) &&
       phoneNumber &&
       typeof phoneNumber === 'string'
     ) {
       const userWithLogin = await User.findOne({ login: { $eq: login } });
       if (userWithLogin) {
+        cleanFiles(req.file.filename);
         return res.status(409).send('User with this login already exists');
       }
 
       const user = await User.create({
         login,
         password: await bcrypt.hash(password, 10),
-        avatar,
+        avatar: req.file.filename,
         phoneNumber: phoneNumber,
       });
       res.status(201).send({ message: 'User created ' + user.login });
     } else {
+      cleanFiles(req.file.filename);
       res.status(400).send('Bad request');
     }
   } catch (err) {
+    cleanFiles(req.file.filename);
     res.status(500).send({ message: err.message });
   }
 };
@@ -63,6 +69,7 @@ exports.login = async (req, res) => {
   }
 };
 
-exports.getUser = async (req, res) => {
-  res.status(200).send({ message: 'User logged!' });
+exports.logout = (req, res) => {
+  req.session.destroy();
+  res.status(200).send({ message: 'User logged out' });
 };
