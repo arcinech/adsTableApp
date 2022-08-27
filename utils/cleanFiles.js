e8bd37450405f892841a8a43a696caf602c7c8eb
Marcin Lech <arcinech@gmail.com>
2022-08-27 21:04:27 +0200

    Fix: cleanFile path

--- utils/cleanFiles.js
+++ utils/cleanFiles.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
+let path = require('path');
+
 const cleanFile = async filename => {
   try {
     if (filename) {
-      await fs.unlinkSync(`${__dirname}/../public/images/${filename}`);
+      await fs.unlinkSync(path.join(__dirname, `../public/uploads/${filename}`));
+      fs.unlinkSync;
     }
   } catch (err) {
     console.log(err);
