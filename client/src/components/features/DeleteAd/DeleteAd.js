03a8f67b17d146fecea8f983863574c7709a50cb
Marcin Lech <arcinech@gmail.com>
2022-09-19 21:23:59 +0200

    Add: requests views for delete Ad

--- client/src/components/features/DeleteAd/DeleteAd.js
+++ client/src/components/features/DeleteAd/DeleteAd.js
@@ -1,22 +1,34 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
-import { deleteAdsRequest } from '../../../redux/reducers/adsRedux';
+import {
+  deleteAdsRequest,
+  getRequest,
+  DELETE_AD,
+} from '../../../redux/reducers/adsRedux';
+import Spinner from 'react-bootstrap/Spinner';
+import Alert from 'react-bootstrap/Alert';
 
 const DeleteAd = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { id } = useParams();
+  const requests = useSelector(state => getRequest(state, DELETE_AD));
 
   useEffect(() => {
     const deleteAction = () => {
       dispatch(deleteAdsRequest(id));
-
       navigate('/');
     };
     deleteAction();
   }, [dispatch, id, navigate]);
-  return null;
+  if (requests.pending === true) {
+    return <Spinner />;
+  } else if (requests.pending === false && requests.success === true) {
+    return null;
+  } else {
+    return <Alert variant='danger'>Error! {requests.error}</Alert>;
+  }
 };
 
 export default DeleteAd;
