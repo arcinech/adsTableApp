39ada5eacf5c7b677c3518479e23e63bd7930eb1
Marcin Lech <arcinech@gmail.com>
2022-09-19 22:11:05 +0200

    Fix: result Search

--- client/src/components/features/SearchResult/SearchResult.js
+++ client/src/components/features/SearchResult/SearchResult.js
@@ -1,27 +1,37 @@
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import AdsList from '../AdsList/AdsList';
-import { getRequest, getSearchResult } from '../../../redux/reducers/adsRedux';
+import {
+  getRequest,
+  getSearchResult,
+  SEARCH_ADS,
+} from '../../../redux/reducers/adsRedux';
 import Spinner from 'react-bootstrap/Spinner';
+import Alert from 'react-bootstrap/Alert';
 
 const SearchResult = () => {
-  const { searchResult } = useParams();
+  const { searchPhrase } = useParams();
 
   const ads = useSelector(getSearchResult);
-  const request = useSelector(getRequest);
-
-  if (request.pending === false && request.success === true && ads.length === 0) {
+  const requests = useSelector(state => getRequest(state, SEARCH_ADS));
+  if (
+    requests.pending === false &&
+    requests.success === true &&
+    (!ads || ads.length === 0)
+  ) {
     return (
       <div>
         <h2>Sorry, no results found</h2>
       </div>
     );
-  } else if (request.pending === true) {
+  } else if (!requests || requests.pending === true) {
     return <Spinner />;
+  } else if (requests.error) {
+    return <Alert variant='danger'>Error! {requests.error}</Alert>;
   } else {
     return (
       <div>
-        <h2>Search results for: {searchResult}</h2>
+        <h2>Search results for: {searchPhrase}</h2>
         <AdsList ads={ads} />
       </div>
     );
