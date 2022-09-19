60e783a039ffebd485903d44215ce86aa20bc07e
Marcin Lech <arcinech@gmail.com>
2022-09-19 21:22:36 +0200

    Fix: adCard component

--- client/src/components/common/AdCard/AdCard.js
+++ client/src/components/common/AdCard/AdCard.js
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { IMGS_URL } from '../../../config';
+import ModalDelete from '../ModalDelete/ModalDelete';
 
 const AdCard = ({
   title,
@@ -16,42 +17,35 @@ const AdCard = ({
   ...params
 }) => {
   const loggedUser = useSelector(({ user }) => user);
-  // console.log(params);
-  // console.log(loggedUser, user);
 
   return (
     <Card style={!params.id ? { width: '18rem' } : { width: '50%' }}>
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
+        <div className='d-flex justify-content-between'>
+          <Card.Title>{title}</Card.Title>
+        </div>
         <Card.Img src={`${IMGS_URL}/${image}`} />
+        <Card.Text>{description}</Card.Text>
         <Card.Text>{price}</Card.Text>
         <Card.Text>{location}</Card.Text>
         <Card.Text>{createdAt}</Card.Text>
-        <Card.Text>{user?.login}</Card.Text>
+        <Card.Text>Author: {user?.login}</Card.Text>
         {!params.id && (
-          <Button as={NavLink} to={`/ads/${_id}`} va riant='primary'>
+          <Button as={NavLink} to={`/ads/${_id}`} variant='primary'>
             Read more
           </Button>
         )}
         {loggedUser?.login === user.login && params.id === _id && (
           <>
             <Button
-              className='me-auto ms-0'
+              className='me-1'
               as={NavLink}
               to={`/ads/edit/${_id}`}
               variant='primary'
             >
               Edit
             </Button>
-            <Button
-              className='ms-auto me-0'
-              as={NavLink}
-              to={`/ads/delete/${_id}`}
-              variant='danger'
-            >
-              Delete
-            </Button>
+            <ModalDelete id={_id} />
           </>
         )}
       </Card.Body>
