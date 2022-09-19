1589ff3d3a16824fd3fec2a78265d85b808418a5
Marcin Lech <arcinech@gmail.com>
2022-09-19 22:12:03 +0200

    Fix: handling requests

--- client/src/redux/reducers/adsRedux.js
+++ client/src/redux/reducers/adsRedux.js
@@ -1,7 +1,7 @@
 import { API_URL } from '../../config';
 
 export const getAds = ({ ads }) => ads.data;
-export const getRequest = ({ ads }) => ads.requests;
+export const getRequest = ({ ads }, name) => ads.requests[name];
 export const getAdById = ({ ads }, id) => ads.data.filter(ad => ad.id === id);
 export const getSearchResult = ({ ads }) => ads.search;
 /* ACTIONS */
@@ -14,11 +14,12 @@ const START_REQUEST = createActionName('START_REQUEST');
 const END_REQUEST = createActionName('END_REQUEST');
 const ERROR_REQUEST = createActionName('ERROR_REQUEST');
 
-const LOAD_ADS = createActionName('LOAD_ADS');
-const ADD_AD = createActionName('ADD_AD');
-const EDIT_AD = createActionName('EDIT_AD');
-const DELETE_AD = createActionName('DELETE_AD');
-const SEARCH_ADS = createActionName('SEARCH_ADS');
+export const LOAD_ADS = createActionName('LOAD_ADS');
+export const ADD_AD = createActionName('ADD_AD');
+export const EDIT_AD = createActionName('EDIT_AD');
+export const DELETE_AD = createActionName('DELETE_AD');
+export const SEARCH_ADS = createActionName('SEARCH_ADS');
+export const CLEAR_SEARCH = createActionName('CLEAR_SEARCH');
 
 export const startRequest = payload => ({ payload, type: START_REQUEST });
 export const endRequest = payload => ({ payload, type: END_REQUEST });
@@ -29,32 +30,32 @@ export const loadAds = payload => ({ payload, type: LOAD_ADS });
 export const editAd = payload => ({ payload, type: EDIT_AD });
 export const deleteAd = payload => ({ payload, type: DELETE_AD });
 export const searchAds = payload => ({ payload, type: SEARCH_ADS });
-
+export const clearSearch = () => ({ type: CLEAR_SEARCH });
 /* THUNKS */
 
 export const loadAdsRequest = () => {
   return async dispatch => {
-    dispatch(startRequest({ name: 'LOAD_ADS' }));
+    dispatch(startRequest({ name: LOAD_ADS }));
     try {
       const options = {
         method: 'GET',
       };
       const res = await fetch(`${API_URL}/api/ads`, options);
       const json = await res.json();
+      console.log(json);
       dispatch(loadAds(json));
-      dispatch(endRequest({ name: 'LOAD_ADS' }));
+      dispatch(endRequest({ name: LOAD_ADS }));
     } catch (err) {
-      dispatch(errorRequest({ name: 'LOAD_ADS', error: err.message }));
+      dispatch(errorRequest({ name: LOAD_ADS, error: err.message }));
     }
   };
 };
 
 export const addAdsRequest = ad => {
   return async dispatch => {
-    dispatch(startRequest({ name: 'ADD_AD' }));
+    dispatch(startRequest({ name: ADD_AD }));
 
     try {
-      console.log(...ad);
       const options = {
         method: 'POST',
         body: ad,
@@ -63,16 +64,16 @@ export const addAdsRequest = ad => {
       const res = await fetch(`${API_URL}/api/ads`, options);
       const json = await res.json();
       dispatch(addAd(json));
-      dispatch(endRequest({ name: 'ADD_AD' }));
+      dispatch(endRequest({ name: ADD_AD }));
     } catch (err) {
-      dispatch(errorRequest({ name: 'ADD_AD', error: err.message }));
+      dispatch(errorRequest({ name: ADD_AD, error: err.message }));
     }
   };
 };
 
 export const editAdsRequest = (ad, id) => {
   return async dispatch => {
-    dispatch(startRequest({ name: 'ADD_AD' }));
+    dispatch(startRequest({ name: EDIT_AD }));
 
     try {
       const options = {
@@ -84,44 +85,44 @@ export const editAdsRequest = (ad, id) => {
       const res = await fetch(`${API_URL}/api/ads/${id}`, options);
       const json = await res.json();
       dispatch(editAd(json));
-      dispatch(endRequest({ name: 'ADD_AD' }));
+      dispatch(endRequest({ name: EDIT_AD }));
     } catch (err) {
-      dispatch(errorRequest({ name: 'ADD_AD', error: err.message }));
+      dispatch(errorRequest({ name: EDIT_AD, error: err.message }));
     }
   };
 };
 
 export const deleteAdsRequest = id => {
   return async dispatch => {
-    dispatch(startRequest({ name: 'ADD_AD' }));
+    dispatch(startRequest({ name: DELETE_AD }));
     try {
       const options = {
         method: 'DELETE',
         credentials: 'include',
       };
-      const res = await fetch.delete(`${API_URL}/api/ads/${id}`, options);
-      const json = res.json();
-      dispatch(deleteAd(json));
-      dispatch(endRequest({ name: 'ADD_AD' }));
+      await fetch(`${API_URL}/api/ads/${id}`, options);
+      dispatch(deleteAd(id));
+      dispatch(endRequest({ name: DELETE_AD }));
     } catch (err) {
-      dispatch(errorRequest({ name: 'ADD_AD', error: err.message }));
+      dispatch(errorRequest({ name: DELETE_AD, error: err.message }));
     }
   };
 };
 
 export const searchAdsRequest = search => {
   return async dispatch => {
-    dispatch(startRequest({ name: 'SEARCH_ADS' }));
+    dispatch(startRequest({ name: SEARCH_ADS }));
     try {
       const options = {
         method: 'GET',
       };
+      console.log(search);
       const res = await fetch(`${API_URL}/api/ads/search/${search}`, options);
       const json = await res.json();
       dispatch(searchAds(json));
-      dispatch(endRequest({ name: 'SEARCH_ADS' }));
+      dispatch(endRequest({ name: SEARCH_ADS }));
     } catch (err) {
-      dispatch(errorRequest({ name: 'SEARCH_ADS', error: err.message }));
+      dispatch(errorRequest({ name: SEARCH_ADS, error: err.message }));
     }
   };
 };
@@ -130,23 +131,34 @@ export const searchAdsRequest = search => {
 
 const initialState = {
   data: [],
-  requests: {},
+  requests: [],
+  search: [],
 };
 
 /* REDUCER */
 
-const adsReducer = (statePart = initialState, action) => {
+const adsReducer = (statePart = initialState, action = {}) => {
   switch (action.type) {
     case LOAD_ADS:
       return { ...statePart, data: [...action.payload] };
     case ADD_AD:
-      return { ...statePart, data: [...statePart.ads, action.payload] };
+      return { ...statePart, data: [...statePart.data, action.payload] };
     case EDIT_AD:
-      return statePart.map(ad => (ad._id === action.payload.id ? action.payload : ad));
+      return {
+        ...statePart,
+        data: statePart.data.map(ad =>
+          ad._id === action.payload.id ? action.payload : ad,
+        ),
+      };
     case DELETE_AD:
-      return statePart.filter(ad => ad._id !== action.payload.id);
+      return {
+        ...statePart,
+        data: statePart.data.filter(ad => ad._id !== action.payload.id),
+      };
     case SEARCH_ADS:
       return { ...statePart, search: [...action.payload] };
+    case CLEAR_SEARCH:
+      return { ...statePart, search: [] };
     case START_REQUEST:
       return {
         ...statePart,
