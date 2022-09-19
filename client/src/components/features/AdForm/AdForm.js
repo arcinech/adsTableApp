5b25edf284e28a36d302a2ac6d4b24f9eb75a728
Marcin Lech <arcinech@gmail.com>
2022-09-19 22:28:08 +0200

    Add: basic form validation

--- client/src/components/features/AdForm/AdForm.js
+++ client/src/components/features/AdForm/AdForm.js
@@ -4,6 +4,7 @@ import Alert from 'react-bootstrap/Alert';
 import Spinner from 'react-bootstrap/Spinner';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
 
 const AdForm = ({ action, actionText, requests, ...props }) => {
   const currentUser = useSelector(({ user }) => user);
@@ -16,6 +17,12 @@ const AdForm = ({ action, actionText, requests, ...props }) => {
     user: props.user ?? currentUser ? currentUser.id : undefined,
     createdAt: props.cretedAt ?? undefined,
   });
+
+  const {
+    register,
+    handleSubmit: validate,
+    formState: { errors },
+  } = useForm();
 
   const handleChange = ({ target }) => {
     const { value, name } = target;
@@ -61,20 +68,31 @@ const AdForm = ({ action, actionText, requests, ...props }) => {
   } else {
     return (
       <>
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={validate(handleSubmit)}>
           <Form.Group className='mb-3' controlId='formTitle'>
             <Form.Label>Title</Form.Label>
             <Form.Control
+              {...register('title', { required: true, minLength: 3, maxLength: 50 })}
               type='text'
               placeholder='Enter title'
               name='title'
               value={adData.title}
               onChange={handleChange}
             />
+            {errors.title && (
+              <small className='d-block form-text text-danger mt-2'>
+                Title is too short or too long!(min.3, max.50)
+              </small>
+            )}
           </Form.Group>
           <Form.Group className='mb-3' controlId='formDescription'>
             <Form.Label>Description</Form.Label>
             <Form.Control
+              {...register('description', {
+                required: true,
+                minLength: 10,
+                maxLength: 500,
+              })}
               as='textarea'
               rows='3'
               placeholder='Enter description'
@@ -82,26 +100,48 @@ const AdForm = ({ action, actionText, requests, ...props }) => {
               name='description'
               onChange={handleChange}
             />
+            {errors.description && (
+              <small className='d-block form-text text-danger mt-2'>
+                Description is too short or too long!(min.10, max.500)
+              </small>
+            )}
           </Form.Group>
           <Form.Group className='mb-3' controlId='formPrice'>
             <Form.Label>Price</Form.Label>
             <Form.Control
+              {...register('price', {
+                required: true,
+                min: 0,
+                max: 1000000,
+                type: 'number',
+              })}
               type='number'
               placeholder='Enter price'
               value={adData.price}
               name='price'
               onChange={handleChange}
             />
+            {errors.price && (
+              <small className='d-block form-text text-danger mt-2'>
+                Wrong price!(min.0, max.1000000)
+              </small>
+            )}
           </Form.Group>
           <Form.Group className='mb-3' controlId='formLocation'>
             <Form.Label>Location</Form.Label>
             <Form.Control
+              {...register('location', { required: true, minLength: 0, maxLength: 50 })}
               type='text'
               placeholder='Enter location'
               value={adData.location}
               name='location'
               onChange={handleChange}
             />
+            {errors.location && (
+              <small className='d-block form-text text-danger mt-2'>
+                Location length is wrong!(min.0, max.50)
+              </small>
+            )}
           </Form.Group>
           <Form.Group className='mb-3' controlId='formImage'>
             <Form.Label>Image</Form.Label>
